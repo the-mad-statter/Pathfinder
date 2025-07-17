87bfa5f9d4ee05fd287a44fcbdefc0ee33ddb7c0
Matthew Schuelke <matt@themadstatter.com>
2025-07-17 13:16:38 -0500

    fix readme task imgs 2

--- readme/render_github_document.R
+++ readme/render_github_document.R
@@ -1,8 +1,16 @@
+debug <- FALSE
+
 file.move <- function(from, to, overwrite = TRUE) {
   if (file.copy(from, to, overwrite))
     unlink(from)
 }
 
 rmarkdown::render("README.Rmd", rmarkdown::github_document())
-unlink("README.html")
+
+if (debug) {
+  file.move("README.html", "../README.html")
+} else {
+  unlink("README.html")
+}
+
 file.move("README.md", "../README.md")
